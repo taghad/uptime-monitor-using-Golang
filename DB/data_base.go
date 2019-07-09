ad3548eba067819d9f510201fd414c23c5618905
mohamadhasan <mhtaghad99@gmail.com>
2019-07-10 01:29:35 +0430

    add insert funcs to data_base.go

--- DB/data_base.go
+++ DB/data_base.go
@@ -3,10 +3,11 @@ package DB
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 func createUserTable(db *sql.DB) {
-	st, err0 := db.Prepare("CREATE TABLE IF NOT EXISTS users(id INTEGER,userName varchar(255),urlNum int)")
+	st, err0 := db.Prepare("CREATE TABLE IF NOT EXISTS users(id INTEGER NOT NULL AUTO_INCREMENT,userName varchar(255),password varchar(20),urlNum int,PRIMARY KEY (id))")
 	if err0 != nil {
 		panic(err0.Error())
 	}
@@ -19,7 +20,7 @@ func createUserTable(db *sql.DB) {
 
 func createUrlTable(db *sql.DB) {
 
-	st, err0 := db.Prepare("CREATE TABLE IF NOT EXISTS urls(id INTEGER,url varchar(255),userName varchar(255),HealthCheck int,respOkTime int, respWarTime int,respCritTime int)")
+	st, err0 := db.Prepare("CREATE TABLE IF NOT EXISTS urls(id INTEGER NOT NULL AUTO_INCREMENT,url varchar(255),userName varchar(255),HealthCheck int,respOkTime int,respWarTime int,respCritTime int,PRIMARY KEY (id))")
 	if err0 != nil {
 		panic(err0.Error())
 	}
@@ -32,7 +33,7 @@ func createUrlTable(db *sql.DB) {
 
 func createRequestsTable(db *sql.DB) {
 
-	st, err0 := db.Prepare("CREATE TABLE IF NOT EXISTS reqs(timestamp date, url_id int,state int,status_code int,respTime int)")
+	st, err0 := db.Prepare("CREATE TABLE IF NOT EXISTS reqs(id int NOT NULL AUTO_INCREMENT,url_id int ,state int,status_code int,respTime int, timestamp date,PRIMARY KEY (id))")
 	if err0 != nil {
 		panic(err0.Error())
 	}
@@ -48,6 +49,8 @@ func ConnectDB(user string, password string) *sql.DB {
 	if err0 != nil {
 		panic(err0.Error())
 	}
+
+	//create tables
 	createUserTable(db)
 	createUrlTable(db)
 	createRequestsTable(db)
@@ -65,6 +68,34 @@ func insertNewURL(db *sql.DB, urlId int, url string, user string, healthCheckTyp
 	}
 
 	_, err1 := st.Exec(urlId, url, user, healthCheckType, respOkTime, respWarTime, respCritTime)
+	if err1 != nil {
+		panic(err1.Error())
+	}
+
+}
+
+func insertNewUser(db *sql.DB, userName string, password string) {
+
+	st, err0 := db.Prepare("insert into users (userName, password, urlNum) values (?,?,?)")
+	if err0 != nil {
+		panic(err0.Error())
+	}
+
+	_, err1 := st.Exec(userName, password, 0)
+	if err1 != nil {
+		panic(err1.Error())
+	}
+
+}
+
+func insertNewReq(db *sql.DB, url_id int, state int, status_code int, respTime int) {
+
+	st, err0 := db.Prepare("insert into reqs (url_id, state, status_code, respTime, timestamp ) values (?,?,?,?,?)")
+	if err0 != nil {
+		panic(err0.Error())
+	}
+
+	_, err1 := st.Exec(url_id, state, status_code, respTime, time.Now().Format("2006-01-02 15:04:05"))
 	if err1 != nil {
 		panic(err1.Error())
 	}
